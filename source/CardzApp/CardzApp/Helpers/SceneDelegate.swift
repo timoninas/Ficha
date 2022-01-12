5a12bfb31c4d922cc4ba7a18cee42c01f0240f0c
Timonin Anton <dotruger.37@gmail.com>
2022-01-12 08:20:58 +0300

    CRDZ AppConfiguration in SceneDelegate

--- source/CardzApp/CardzApp/Helpers/SceneDelegate.swift
+++ source/CardzApp/CardzApp/Helpers/SceneDelegate.swift
@@ -19,71 +19,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         fillDataBaseIfNeeded()
         
-        let todayViewController = DailyWordsBuilder.build()
-        if let todayIconData = UIImage.todayIcon?.pngData(),
-           let todayIconFilledData = UIImage.todayIconFilled?.pngData() {
-            let image = UIImage(data: todayIconData, scale: 13.0)
-            let imageFilled = UIImage(data: todayIconFilledData, scale: 13.0)
-            let item = UITabBarItem(title: "Daily", image: image, selectedImage: imageFilled)
-            todayViewController.tabBarItem = item
-        }
-        
-        let learnViewController = LearnNewWordzBuilder.build()
-        if let graduateIconData = UIImage.graduateIcon?.pngData() ,
-           let graduateIconFilledData = UIImage.graduateIconFilled?.pngData() {
-            let image = UIImage(data: graduateIconData, scale: 13.0)
-            let imageFilled = UIImage(data: graduateIconFilledData, scale: 13.0)
-            let item = UITabBarItem(title: "Learn", image: image, selectedImage: imageFilled)
-            learnViewController.tabBarItem = item
-        }
-        
-        let favouriteViewController = FavouriteWordzBuilder.build()
-        if let favouriteIconData = UIImage.starIcon?.pngData(),
-           let favouriteIconFilledData = UIImage.starIconFilled?.pngData() {
-            let image = UIImage(data: favouriteIconData, scale: 13.5)
-            let imageFilled = UIImage(data: favouriteIconFilledData, scale: 13.5)
-            let item = UITabBarItem(title: "Favourite", image: image, selectedImage: imageFilled)
-            favouriteViewController.tabBarItem = item
-        }
-        
-        let devViewController = DevDebugBuilder.build()
-        if let decIconData = UIImage.devIcon?.pngData(),
-           let devIconFilledData = UIImage.devIconFilled?.pngData() {
-            let image = UIImage(data: decIconData, scale: 17.0)
-            let imageFilled = UIImage(data: devIconFilledData, scale: 13.5)
-            let item = UITabBarItem(title: "Developer", image: image, selectedImage: imageFilled)
-            devViewController.tabBarItem = item
-        }
-        let devNavigation = UINavigationController(rootViewController: devViewController)
-        
-        let tabBar = UITabBarController()
-        var viewControllers = [todayViewController, learnViewController, favouriteViewController]
+        let appConfigurator = AppConfigurator()
+        var tabBarModules: [AppConfigurator.TabBarModule] = [
+            .daily,
+            .learn,
+            .favourite
+        ]
         #if DEBUG
-        viewControllers.append(devNavigation)
+        tabBarModules.append(.devDebug)
         #endif
-        tabBar.setViewControllers(viewControllers, animated: true)
-        tabBar.selectedViewController = todayViewController
-        tabBar.selectedIndex = 0
-        tabBar.tabBar.isHidden = false
-        tabBar.tabBar.isTranslucent = true
-        
-        UITabBar.appearance().backgroundColor = .white
-        UITabBar.appearance().tintColor = .mysteryShack
-        UITabBar.appearance().unselectedItemTintColor = .gendalfGray
-        
-        let bounds = tabBar.tabBar.bounds as CGRect
-        let visualEffectView = UIView() //UIVisualEffectView(effect: UIBlurEffect(style: .light))
-        visualEffectView.frame = bounds
-        visualEffectView.backgroundColor = .gendalf
-        visualEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        tabBar.tabBar.insertSubview(visualEffectView, at: 0)
-        
-        UINavigationBar.appearance().backgroundColor = .gendalf
-        UINavigationBar.appearance().barTintColor = .gendalf
-        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.nazgul]
         
         window?.windowScene = windowScene
-        window?.rootViewController = tabBar
+        window?.rootViewController = appConfigurator.buildTabBars(tabBarModules: tabBarModules)
         window?.makeKeyAndVisible()
     }
     
--- source/Frameworks/Hobbiton/Sources/Hobbiton/NavigationController.swift
+++ source/Frameworks/Hobbiton/Sources/Hobbiton/NavigationController.swift
@@ -17,4 +17,5 @@ public class NavigationController : UINavigationController {
         
         return .darkContent
     }
+    
 }
